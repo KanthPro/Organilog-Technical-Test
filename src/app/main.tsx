3b53968c2966215c6f7e7da15c431800042b72f8
KanthPro <pro.anthony.chevallier@gmail.com>
2026-02-02 18:59:25 +0100

    feat: add back persisted store

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -2,8 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import "./index.css";
-import App from "./App.tsx";
-import { store } from "./store";
+import { store, subscribePersist } from "./store";
+import { App } from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -12,3 +12,5 @@ createRoot(document.getElementById("root")!).render(
     </Provider>
   </StrictMode>,
 );
+
+subscribePersist();
