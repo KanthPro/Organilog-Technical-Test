59466867b30534adb62ef96044d51f8a1329028f
KanthPro <pro.anthony.chevallier@gmail.com>
2026-02-02 19:16:09 +0100

    feat: add load more button

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import { useAppDispatch, useAppSelector } from "./hooks";
 import type { AppState } from "./store";
-import { isEmpty, map } from "lodash";
+import { find, isEmpty, map, size } from "lodash";
 import { useEffect, useState } from "react";
 import { getProducts } from "./products.thunks";
 import type { Product } from "./products";
@@ -9,20 +9,27 @@ import type { Product } from "./products";
 export const App = () => {
   const dispatch = useAppDispatch();
   const products = useAppSelector((state: AppState) => state.products);
+  console.log("products", products);
 
   const [opened, setOpened] = useState(false);
   const [selectedProduct, setSelectedProduct] = useState<Product | null>(null);
+  const [offset, setOffset] = useState(size(products.products) || 0);
 
   useEffect(() => {
     if (isEmpty(products.products) && products.loaded === false) {
-      dispatch(getProducts());
+      dispatch(getProducts({ limit: 5, offset: 0 }));
     }
-  }, [dispatch, products.loaded, products.products]);
+  }, [dispatch, offset, products.loaded, products.products]);
 
   const handleSelectProduct = (productId: number) => {
-    const product = products.products.find((p) => p.id === productId);
+    const product = find(products.products, (p) => p.id === productId);
     setOpened(true);
     setSelectedProduct(product || null);
+  };
+
+  const loadProducts = () => {
+    setOffset(offset + 5);
+    dispatch(getProducts({ limit: 5, offset: offset + 5 }));
   };
 
   return (
@@ -40,6 +47,9 @@ export const App = () => {
             </div>
           );
         })}
+        <div className="load-more">
+          <button onClick={loadProducts}>Load more</button>
+        </div>
       </div>
       <div className={`panel ${opened ? "opened" : "closed"}`}>
         <div className="panel-close" onClick={() => setOpened(false)}>
