cf81718a0a0744a8495d598ab5dfbf1e839333a0
KanthPro <pro.anthony.chevallier@gmail.com>
2026-02-02 19:16:18 +0100

    feat: remove unused types

--- src/app/products.ts
+++ src/app/products.ts
@@ -10,20 +10,3 @@ export type Product = {
   thumbnail: string;
   category: string;
 };
-
-export interface ProductDto {
-  id: number;
-  title: string;
-  price: number;
-  description: string;
-  rating: number;
-  stock: number;
-  brand: string;
-  images: string[];
-  thumbnail: string;
-  category: string;
-}
-
-export type ProductsRequest = {
-  limit?: number;
-};
